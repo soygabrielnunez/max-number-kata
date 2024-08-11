903a7130a4fac678ccad525e17d9dbc82132a5a8
Gabriel <gabriel.nzn@gmail.com>
2024-08-11 18:51:14 +0100

    ✅ should get the max number in the array

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export function getMaxNumberFrom (numbers) {
+  let maxNumber = 0
+  let size = numbers.length
+  for(let i = 0; i < size; i++) {
+    if(maxNumber < numbers[i]) {
+      maxNumber = numbers[i]
+    }
+  }
+  return maxNumber
 }
